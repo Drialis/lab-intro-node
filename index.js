bbb94a81839185cfa88ff9bce3b11ff601ae99fb
Paloma Rehecho <paloma_re@hotmail.com>
2024-04-29 15:05:25 +0100

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,82 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
 
-  get(pos) {}
+    this.items = [...this.items, item]
+    this.items.sort((a, b) => {
+      a - b
+      return a - b
+    })
+    this.length++
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds")
+    }
+    return this.items[pos]
+  }
 
-  min() {}
+  max() {
+    let maxValue = this.items[0]
 
-  sum() {}
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
 
-  avg() {}
+    this.items.forEach((eachValue) => {
+      if (eachValue > maxValue) {
+        maxValue = eachValue;
+      }
+    })
+
+    return maxValue;
+  }
+
+
+  min() {
+
+    let minValue = this.items[0]
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+
+    this.items.forEach((eachValue) => {
+      if (eachValue < minValue) {
+        minValue = eachValue
+      }
+    })
+    return minValue
+  }
+
+  sum() {
+
+    let sum = 0
+    this.items.forEach((eachValue) => {
+      sum += eachValue
+    })
+    return sum
+  }
+
+  avg() {
+
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+
+    let sum = 0
+    this.items.forEach((eachValue) => {
+      sum += eachValue
+    })
+    return sum / this.length
+  }
 }
+
+
 
 module.exports = SortedList;
